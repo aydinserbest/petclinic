58a7c311bfa4a27ca6f73137c66c1fe87fd4b309
aydinSerbest <aydinserbest34@gmail.com>
2024-04-17 11:36:13 +0200

    updated

--- src/test/java/org/example/pettypes/DisplayedPetTypesList.java
+++ src/test/java/org/example/pettypes/DisplayedPetTypesList.java
@@ -8,11 +8,12 @@ import java.util.stream.Collectors;
 
 public class DisplayedPetTypesList extends UIInteractionSteps {
     public List<String> thatAreVisible(){
-         return findAll(By.name("pettype_name")).stream()
-                .map(element -> element.getAttribute("value"))
-                .collect(Collectors.toList());
+        return $$(By.name("pettype_name").map(field -> field.getValue())
+//         return findAll(By.name("pettype_name")).stream()
+//                .map(element -> element.getAttribute("value"))
+//                .collect(Collectors.toList());
 
-
+        //there is no visible text in the html so this is not working, we need to use value-getAttribute like above
         //return $$(By.name("pettype_name")).texts();
         //return findAll(By.name("pettype_name")).texts();
     }
